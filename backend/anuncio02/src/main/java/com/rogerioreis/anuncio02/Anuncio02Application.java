98c3e706e94670e4eb2ac1ca82203ce6a040f2d5
Rogerio Reis <rogerio.rtr46@gmail.com>
2021-05-12 11:19:35 -0300

    projeto spring gerado

---
+++ backend/anuncio02/src/main/java/com/rogerioreis/anuncio02/Anuncio02Application.java
@@ -0,0 +1,13 @@
+package com.rogerioreis.anuncio02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Anuncio02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Anuncio02Application.class, args);
+	}
+
+}
---
+++ backend/anuncio02/src/test/java/com/rogerioreis/anuncio02/Anuncio02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogerioreis.anuncio02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Anuncio02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
